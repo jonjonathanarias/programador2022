251a0d455298646da4edd1a7c76fff5b30070460
your_github_username <gabrielnietodejesus@gmail.com>
2022-08-10 21:42:46 -0300

    Ejercicio 10

---
+++ ListadeClientes.py
@@ -0,0 +1,11 @@
+# Crear una lista denominada “Clientes” con los nombres de los diferentes dueños de perros.
+#    Juan,  Mario,  Ariel,  Josefina,  Marianella.
+# Ordenarla alfabéticamente y mostrarla por pantalla.
+
+
+Clientes = ["Juan", "Mario", "Ariel", "Josefina", "Marianella"]
+
+print("Lista de clientes desordenada: ", Clientes)
+print("Lista de clientes ordenada alfabéticamente: ", sorted(Clientes))
+
+
