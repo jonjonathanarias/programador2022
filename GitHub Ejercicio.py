f90dcad4ea37a49470511783fd4fe8ae498af474
gabrielbustos23 <106617553+gabrielbustos23@users.noreply.github.com>
2022-08-12 18:25:19 -0300

    Actividad 1 Python

---
+++ GitHub Ejercicio.py
@@ -0,0 +1,14 @@
+DNI = 28957346
+NOMBRE = ["Juan"]
+APELLIDO = ["Perez"]
+TELEFONO = ["4789689"]
+DIRECCION = ["Belgrano 101"]
+
+numero_1 = 26000000
+
+DNI = int(input('Ingresa el DNI: '))
+
+if DNI > numero_1:
+    print (TELEFONO) 
+else:
+    print ("Error el Numero es Menor")
